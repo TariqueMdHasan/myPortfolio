b20a6d322928637ceaf35eca60c3da8171f66941
Md Tarique Hasan <TariqueMdHasan@gmail.com>
2025-02-28 16:55:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Navbar from './component/Navbar'
+
+function App() {
+  return (
+    <div>
+      {/* <button className='nav-btn'>=</button> */}
+      <Navbar />
+      {/* hii */}
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,78 @@
+import { IoMenu } from "react-icons/io5";
+import { ImCross } from "react-icons/im";
+import { IoMdSunny } from "react-icons/io";
+import { MdDarkMode } from "react-icons/md";
+
+
+
+
+
+
+import React, { useState } from "react";
+import "./Navbar.css";
+import { useTheme } from "../ThemeCotext";
+import { motion, AnimatePresence } from "framer-motion"; 
+
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <div>
+      {isOpen && (
+        <div
+          onClick={() => setIsOpen(false)}
+          className="Nav-overlay"
+        ></div>
+      )}
+
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            className="Navbar"
+            initial={{ x: "-12rem" }} 
+            animate={{ x: "0rem" }} 
+            exit={{ x: "-12rem" }} 
+            transition={{ type: "tween", duration: 0.3 }}
+          >
+            <div>
+            <button
+                className="Nav-Close-btn"
+             onClick={() => setIsOpen(false)}><ImCross /></button>
+            <button onClick={toggleTheme} className="theme-btn">
+              {theme === "light" ? <MdDarkMode /> : <IoMdSunny />
+
+              }
+            </button>
+            </div>
+            <div className="nav-list">
+                
+                    <div><p>Home</p></div>
+                    <div><p>Introduction</p></div>
+                    <div><p>Skills</p></div>
+                    <div><p>Projects</p></div>
+                    <div><p>Contacts</p></div>
+                
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+
+
+        
+
+      {!isOpen && (
+        
+            <button 
+            className="Nav-Open-btn"
+            
+            onClick={() => setIsOpen(true)}><IoMenu /></button>
+        
+      )}
+    </div>
+  );
+}
+
+export default Navbar;
+
